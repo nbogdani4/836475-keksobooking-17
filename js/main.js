9014f79d03b90aaf83814a347d07fcd644d4a563
N.Bogdanov <nikolaj.bogdanov@24com.lv>
2019-07-18 00:58:58 +0300

    Сделал перемещение главного пина по всей зоне .map

--- js/main.js
+++ js/main.js
@@ -162,11 +162,48 @@ var getFragmentWithPins = function (valueLength) {
 
 var advertisementsDataArray = genDataObjectsArray(ADVERTISEMENTS_COUNT);
 
-// При клике на главную метку, обработчик событий делает карту, фильтр, форму и поля формы активной
-mapPinMain.addEventListener('mouseup', function () {
-  cityMap.classList.remove('map--faded');
-  adForm.classList.remove('ad-form--disabled');
-  delAttributeDisabled(adForm.children);
-  delAttributeDisabled(mapfilterForm.children);
-  document.querySelector('.map__pins').appendChild(getFragmentWithPins(advertisementsDataArray.length));
+// // При клике на главную метку, обработчик событий делает карту, фильтр, форму и поля формы активной
+// mapPinMain.addEventListener('mouseup', function () {
+//   cityMap.classList.remove('map--faded');
+//   adForm.classList.remove('ad-form--disabled');
+//   delAttributeDisabled(adForm.children);
+//   delAttributeDisabled(mapfilterForm.children);
+//   document.querySelector('.map__pins').appendChild(getFragmentWithPins(advertisementsDataArray.length));
+// });
+
+mapPinMain.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+    mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    cityMap.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  cityMap.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
