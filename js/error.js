b29da452cfa1b6c52fda2dd15c5548e53f020c6c
N.Bogdanov <nikolaj.bogdanov@24com.lv>
2019-08-03 15:27:03 +0300

    Добавил возможность закрыть ошибку с помощью нажатия ESC или кликом мышки

--- js/error.js
+++ js/error.js
@@ -4,10 +4,36 @@
 
   var main = document.querySelector('main');
 
+  // Функция удаляет сообщение об ошибке и сбрасывает окно в неактивное состояние
+  var onCloseError = function () {
+    window.form.disableForm();
+    window.filter.disableFilter();
+    window.map.disableMap();
+    main.removeChild(main.querySelector('.error'));
+    document.removeEventListener('click', onErrorMouseClick);
+    document.removeEventListener('keydown', onErrorEscPress);
+  };
+
+  // Функция проверяет клик мышки и вызывает функцию закрытия сообщения об ошибке
+  var onErrorMouseClick = function (evt) {
+    if ((evt.target.className === 'error') || (evt.target.className === 'error__button')) {
+      onCloseError();
+    }
+  };
+
+  // Функция проверяет правильная ли была нажата кнопка, и вызывает функцию закрытия сообщения об ошибке
+  var onErrorEscPress = function (evt) {
+    window.util.isEscEvent(evt, onCloseError);
+  };
+
+  // Функция генерирует шаблон ошибки
   var onError = function (errorMessage) {
     var node = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
     node.querySelector('.error__message').textContent = errorMessage;
     main.appendChild(node);
+
+    document.addEventListener('click', onErrorMouseClick);
+    document.addEventListener('keydown', onErrorEscPress);
   };
 
   window.error = {
--- js/util.js
+++ js/util.js
@@ -18,6 +18,24 @@
     element.disabled = false;
   };
 
+  window.util = (function () {
+    var ESC_KEYCODE = 27;
+    var ENTER_KEYCODE = 13;
+
+    return {
+      isEscEvent: function (evt, action) {
+        if (evt.keyCode === ESC_KEYCODE) {
+          action();
+        }
+      },
+      isEnterEvent: function (evt, action) {
+        if (evt.keyCode === ENTER_KEYCODE) {
+          action();
+        }
+      }
+    };
+  })();
+
   window.util = {
     elementMap: elementMap,
     mainPin: mainPin,
